3e9b958d486626b336118567e4b262c97c152f81
Ilham Gusnul <rilham2612@gmail.com>
2025-12-14 03:30:35 +0700

    chore: conditionally enable the wayfinder plugin only when the Vite command is 'serve'.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
@@ -17,11 +17,11 @@ export default defineConfig({
             },
         }),
         tailwindcss(),
-        wayfinder({
+        command === 'serve' ? wayfinder({
             formVariants: true,
-        }),
+        }) : null,
     ],
     esbuild: {
         jsx: 'automatic',
     },
-});
+}));
